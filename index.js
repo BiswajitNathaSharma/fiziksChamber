a210c0d4877c8a9c41b9c6e0b9d33ee00d30d380
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-08-27 09:10:51 +0530

    js added

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 var typed = new Typed("#element", {
-    strings: ["AFFORDABLE", "PRACTICAL", "EASY", "INTERACTIVE", "RELEVANT", "CONVENIENT"],
-    typeSpeed: 100,
-    backspace: 100,
-    loop: true,
+  strings: [
+    "AFFORDABLE",
+    "PRACTICAL",
+    "EASY",
+    "INTERACTIVE",
+    "RELEVANT",
+    "CONVENIENT",
+  ],
+  typeSpeed: 100,
+  backspace: 100,
+  loop: true,
+});
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("class");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
   });
+  menuicon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
-  let sections = document.querySelectorAll('section')
-  let navLinks = document.querySelectorAll('header nav a')
+let menuicon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".nav-bar");
 
-  window.onscroll = () =>{
-    sections.forEach(sec => {
-      let top = window.scrollY;
-      let offset = sec.offsetTop - 150; 
-      let height = sec.offsetHeight;
-      let id = sec.getAttribute('class')
-      
-      if(top >= offset && top < offset + height){
-        navLinks.forEach(links =>{
-          links.classList.remove('active');
-
-          document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-        });
-      }
-    });
-    menuicon.classList.remove('bx-x')
-    navbar.classList.remove('active')
-    
-
-  }
-
-  let menuicon = document.querySelector('#menu-icon')
-  let navbar = document.querySelector('.nav-bar')
-
-  menuicon.onclick =() =>{
-    menuicon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
-    
-  }
-
+menuicon.onclick = () => {
+  menuicon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
