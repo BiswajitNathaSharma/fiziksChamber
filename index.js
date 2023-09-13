fac71ed7e5aefd2c6e8f5441ac77dad2de15b610
BiswajitNathaSharma <122850529+BiswajitNathaSharma@users.noreply.github.com>
2023-09-13 21:32:12 +0530

    contact form ready

--- index.js
+++ index.js
@@ -43,31 +43,132 @@ menuicon.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-
 const modal = document.querySelector("#modal");
 const openModal = document.querySelector(".open-button");
 const closeModal = document.querySelector(".close-button");
 
-function show_model(){
+function show_model() {
   modal.showModal();
 }
-function close_model(){
+function close_model() {
   modal.close();
 }
 
 function changeIframe(change) {
-  show_model()
-  document.getElementById('myiframe').src = change;
- }
-  
+  show_model();
+  document.getElementById("myiframe").src = change;
+}
+
 let makeDialogFullScreen = document.getElementById("modal");
 let fullScreenBtn = document.getElementById("fullScreen");
-fullScreenBtn.addEventListener("click",()=>{
-  makeDialogFullScreen.classList.add("modal-full")
-  
-  
-})
+fullScreenBtn.addEventListener("click", () => {
+  makeDialogFullScreen.classList.add("modal-full");
+});
 let halfScreenBtn = document.getElementById("halfScreen");
-halfScreenBtn.addEventListener("click",()=>{
-  makeDialogFullScreen.classList.remove("modal-full")
-})
+halfScreenBtn.addEventListener("click", () => {
+  makeDialogFullScreen.classList.remove("modal-full");
+});
+//form submit
+let form = document.getElementById("form");
+ form.addEventListener("submit", validateSubmit);
+function sendmail() {
+  
+  let numbers = document.getElementById("number").value;
+  let messages = document.getElementById("message").value;
+  let subject = document.getElementById("subject").value;
+  let email = document.getElementById("email").value;
+  let body = `my email: ${email} <br>phone number: ${numbers} <br> message: ${messages}`;
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "sharmaoncourse@gmail.com",
+    Password: "B33D9F91BB922C2DC7D2B52C77A1E9BF2F68",
+    To: "lipubiswajit109@gmail.com",
+    From: "sharmaoncourse@gmail.com",
+    Subject: subject,
+    Body: body,
+  }).then((message) => openPopup(message));
+}
+
+//popup
+let popup = document.getElementById("popup");
+let pop_p = document.getElementById("popup-p");
+let pop_h = document.getElementById("popup-h2");
+let myform = document.getElementById("form");
+function openPopup(message) {
+  if (message.includes("OK")) {
+    pop_p.innerHTML = `Message send successfully`;
+  } else {
+    pop_h.innerHTML = `Sorry!`;
+    pop_p.innerHTML = `Message can't send, due to: ${message}`;
+  }
+  popup.classList.add("open-popup");
+}
+function closePopup() {
+  popup.classList.remove("open-popup");
+  myform.reset();
+}
+
+// form validation
+
+let errors = document.getElementById("errors");
+let fixThis = document.getElementById("fix-errors");
+
+function validateName() {
+  let name = document.getElementById("name").value;
+  if (name.length == 0) {
+    errors.innerHTML = "name is required!";
+    return false;
+  }
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    errors.innerHTML = "Enter full name";
+    return false;
+  } 
+    errors.innerHTML = "";
+    return true;
+  
+}
+function validatePhone() {
+  let phone = document.getElementById("number").value;
+  if (phone.length == 0) {
+    errors.innerHTML = "phone number is required";
+    return false;
+  }  if (phone.length !== 10) {
+    errors.innerHTML = "phone number should be 10 digits";
+    return false;
+  } if (!phone.match(/^[0-9]{10}$/)) {
+    errors.innerHTML = "Only digits are allowed";
+    return false;
+  } 
+    errors.innerHTML = "";
+    return true;
+  
+}
+function validateEmail() {
+  let email = document.getElementById("email").value;
+
+  if (email.length == 0) {
+    errors.innerHTML = "Email shouldn't be empty";
+    return false;
+  }
+  if (!email.match(/^[a-z0-9](\.?[a-z0-9]){3,}@[Gg][Mm][Aa][Ii][Ll]\.com$/)) {
+    errors.innerHTML = "Enter a valid email, Only gmail alowed";
+    return false;
+  }
+
+  errors.innerHTML = "";
+  return true;
+}
+
+function validateSubmit() {
+  if (!validateEmail() || !validateName() || !validatePhone()) {
+    fixThis.style.display = "block";
+    fixThis.innerHTML = "please fix the error to submit.";
+    setTimeout(() => {
+      fixThis.style.display = "none";
+    }, 4000);
+    return false;
+  }
+  else
+  sendmail()
+  return true
+}
